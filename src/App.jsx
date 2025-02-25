53871c851e8e25d7d2b9ca092cc989d51fead374
omarfattah44 <omarmfattah44@gmail.com>
2025-02-25 14:26:14 -0800

    Final draft

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import { useRef } from 'react'
 import './App.css'
 
 
-
 function App() {
-  
+  //useState with list of all 151 poke with name and url
   const [poke, setPoke] = useState([])
+  //useState with expanded list of all pokemon including name stats moves and images
   const [pokeData, setPokeData] = useState([])
+  //use state that is set true when the page starts loading and is set back to false when the page is done loading
   const [loading, setLoading] = useState(false);
+  //use states that check if a fetch function is done running before another function starts
   const [firstDone, setFirstDone] = useState(false);
   const [secondDone, setSecondDone] = useState(false);
+  //is set to true when a pokemon is clicked on it then displays that pokemonds data
   const [displayPokemon, setDisplayPokemon] = useState(false);
+  //use state that stores the id of the selected pokemon for data grabbing
   const [chosenPokemon, setChosenPokemon] = useState(0);
 
+  //inital fetch that grabs all 151 pokemon
   const fetchData = async () => {
     let res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
     let data = await res.json()
@@ -36,10 +41,12 @@ function App() {
     }
   }, [firstDone]);
 
+  //once all data for the 151 pokemon is fetched it calls the pokeCard function to make all the cards to be displayed
   useEffect(() => {
     pokeCard()
   }, [secondDone])
 
+  //this iterates over all 151 pokemon and fetches the data for each one and adds it to an array
   const individualPoke = async () => {
     var resultArry = []
     for(let i of poke.results) {
@@ -52,6 +59,8 @@ function App() {
     setSecondDone(true)
   }
 
+
+  //this takes all the completed fetch data for the pokemon and makes it a card to be displayed
   function pokeCard(){
     const resultsContainer = document.getElementById("results");
     pokeData.forEach(pokemon => {
@@ -90,20 +99,19 @@ function App() {
 
   
   
-
+  //checks if a pokemon has been been chosen. If so it displays the specialized information from that pokemon.
   if(displayPokemon){
-
+    //sets an array of types for the pokemon to be dispayed later in the code
     var typeArry = [];
     for(let i of pokeData[chosenPokemon -1].types){
       typeArry.push(i.type.name)
     }
 
+    //sets an array of moves to be displayed later in the code
     var moveArry = [];
     for(let i of pokeData[chosenPokemon -1].moves){
       moveArry.push(i.move.name)
     }
-
-    console.log(moveArry)
     
     let displayPokemonHTML = (
       <> 
@@ -141,6 +149,8 @@ function App() {
     return displayPokemonHTML
   }
 
+
+  //checks if the page is loading if so it sends out a loading h1 (this doesnt happen cause the page is too fast)
   if (loading){
     return (
       <div>
@@ -148,6 +158,8 @@ function App() {
       </div>
     )
   }
+
+  //default html for the page it shows all 151 pokemon with all of the cards. 
   return (
     <>
     <header><h1 id="title">PokeMANS</h1></header>
@@ -162,10 +174,6 @@ function App() {
       </div>
         <section id="results"></section>
       </div>
-        
-      
-        
-      
     </>
   )
 }
